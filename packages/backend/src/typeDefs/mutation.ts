b9a7044e0d48c849614d7d8184959b77576e775a
Vitu2002 <contact@vitu2002.dev>
2025-02-18 14:39:03 +0100

    feat(login): add gql types for login mutation

--- packages/backend/src/typeDefs/mutation.ts
+++ packages/backend/src/typeDefs/mutation.ts
@@ -3,5 +3,18 @@ import gql from 'graphql-tag'
 export default gql`
   type Mutation {
     mutationTest(test: Boolean): Boolean
+    mutationLogin(email: String!, password: String!): LoginResponse
+  }
+
+  type LoginResponse {
+    message: String
+    success: Boolean
+    user: User
+  }
+
+  type User {
+    email: String!
+    name: String!
+    company: String!
   }
 `
